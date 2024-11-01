8446ab3f3d38514f717a5466c9456e0ef142c9aa
Nick Sacco <nicksacco10@gmail.com>
2024-11-01 15:28:28 -0600

    expanded tests for seeing paintings on landing page and updated index controller action

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,5 @@
 class LandingController < ApplicationController
   def index
-    
+    @paintings = Painting.all
   end
 end
--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -9,12 +9,16 @@ RSpec.describe 'Landing Page', type: :feature do
     end
 
     it 'displays all paintings on the landing page' do
-      painting1 = Painting.create(title: 'Painting 1', price: 100, image: 'image1.jpg', description: 'Description 1')
-      painting2 = Painting.create(title: 'Painting 2', price: 150, image: 'image2.jpg', description: 'Description 2')
-      painting3 = Painting.create(title: 'Painting 3', price: 286795, image: 'image3.jpg', description: 'Description 3')
+      painting1 = Painting.create(title: 'Painting 1', price: 100, image: 'pics/matt_1.jpg', description: 'Description 1')
+      painting2 = Painting.create(title: 'Painting 2', price: 150, image: 'pics/matt_2.jpg', description: 'Description 2')
+      painting3 = Painting.create(title: 'Painting 3', price: 286795, image: 'pics/matt_3.jpg', description: 'Description 3')
+      painting4 = Painting.create(title: 'Painting 4', price: 286795, image: 'pics/matt_4.jpg', description: 'Description 3')
 
       visit root_path
       expect(page).to have_content(painting1.attributes.values)
+      expect(page).to have_content(painting2.attributes.values)
+      expect(page).to have_content(painting3.attributes.values)
+      expect(page).to have_content(painting4.attributes.values)
     end
   end
 end
