8aea46564b4c815b0b8d28cdcb1851718a64dcd6
Nick Sacco <nicksacco10@gmail.com>
2024-11-29 16:12:24 -0700

    replaced welcome message tests with signature tests

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Landing Page', type: :feature do
   describe 'user visits landing page' do
 
-    it 'sees a welcome message' do
+    it 'displays signature on top and bottom of the page' do
       visit root_path
-      expect(page).to have_content('Welcome message or signature here')
+      expect(page).to have_css("img[src*='pics/sig2.png']")
+      expect(page).to have_css("img[src*='pics/sig1.png']")
     end
 
     it 'displays all paintings on the landing page' do
